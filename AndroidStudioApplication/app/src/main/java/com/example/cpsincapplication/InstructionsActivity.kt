879cbacc14bf852918a0706193a55075cadc60ad
ColtonOsterlund <coltonericosterlund@gmail.com>
2020-05-05 16:26:38 -0600

    adding instructions activity file

---
+++ AndroidStudioApplication/app/src/main/java/com/example/cpsincapplication/InstructionsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cpsincapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InstructionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instructions)
+    }
+}
--- AndroidStudioApplication/app/src/main/java/com/example/cpsincapplication/MainActivity.kt
+++ AndroidStudioApplication/app/src/main/java/com/example/cpsincapplication/MainActivity.kt
@@ -3,19 +3,22 @@ package com.example.cpsincapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var websiteHyperlinkTextView: TextView
-    lateinit var findDeviceBtn: Button
-    lateinit var runTestBtn: Button
-    lateinit var logbookBtn: Button
-    lateinit var accountBtn: Button
-    lateinit var settingsBtn: Button
+    lateinit var instructionsBtnTextView: TextView
+    lateinit var findDeviceBtn: ImageButton
+    lateinit var runTestBtn: ImageButton
+    lateinit var logbookBtn: ImageButton
+    lateinit var accountBtn: ImageButton
+    lateinit var settingsBtn: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,8 @@ class MainActivity : AppCompatActivity() {
 
     fun setViewComponents(){
         websiteHyperlinkTextView = findViewById(R.id.textView7)
+        websiteHyperlinkTextView.movementMethod = LinkMovementMethod.getInstance();
+        instructionsBtnTextView = findViewById(R.id.textView8)
         findDeviceBtn = findViewById(R.id.imageButton)
         runTestBtn = findViewById(R.id.imageButton2)
         logbookBtn = findViewById(R.id.imageButton3)
@@ -35,13 +40,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setButtonListeners(){
+        instructionsBtnTextView.setOnClickListener(instructionsTextViewOnClickListener)
         findDeviceBtn.setOnClickListener(findDeviceBtnOnClickListener)
         runTestBtn.setOnClickListener(runTestBtnOnClickListener)
         logbookBtn.setOnClickListener(logbookBtnOnClickListener)
         accountBtn.setOnClickListener(accountBtnOnClickListener)
         settingsBtn.setOnClickListener(settingsBtnOnClickListener)
     }
-    
+
+    val instructionsTextViewOnClickListener = object: View.OnClickListener {
+        override fun onClick(view: View) {
+            val intent = Intent(this@MainActivity, InstructionsActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     val findDeviceBtnOnClickListener = object: View.OnClickListener {
         override fun onClick(view: View) {
